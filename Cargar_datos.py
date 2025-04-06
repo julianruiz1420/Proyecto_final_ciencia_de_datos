9e1a77a0f57cda2941c1ee6aa9a5b5133ab54239
julianruiz1420 <julian.ruiz1420@gmail.com>
2025-04-06 18:33:28 -0500

    update readme.md

--- Cargar_datos.py
+++ Cargar_datos.py
@@ -1,14 +1,5 @@
 import pandas as pd
-import numpy as np
 
-url = (r"D:\3_Especialización_en_ciencia_de_datos\2. material de clase, cursos y materias\ciencia de datos\Proyecto_final_chrum\Trabajo final 2 - Churn-20250329\Churn_Modelling\Churn_Modelling.csv")
+df_ppal = pd.read_csv("Churn_Modelling.csv")
 
-df_ppal = pd.read_csv(url)
-
-# print(df_ppal.head)
-
-# print(df_ppal.columns)
-
-print(df_ppal.info())
-
-print(df_ppal.describe())
+print(df_ppal.head())
