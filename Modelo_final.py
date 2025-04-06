397ebbd84380885120668b0d0ca8b79e402d2d3c
julianruiz1420 <julian.ruiz1420@gmail.com>
2025-04-06 18:11:17 -0500

    update readmed.mod

---
+++ Modelo_final.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+url = "D:\3_Especialización_en_ciencia_de_datos\2. material de clase, cursos y materias\ciencia de datos\Proyecto_final_chrum\Untitled-1.txt"
+
+df_ppal = pd.read.txt(url)
+
+print(df_ppal)
