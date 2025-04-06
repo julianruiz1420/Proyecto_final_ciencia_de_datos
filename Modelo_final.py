0a7d6b752e6a8b578edaac767b034c56f05d4824
julianruiz1420 <julian.ruiz1420@gmail.com>
2025-04-06 18:17:47 -0500

    update readme.md

--- Modelo_final.py
+++ Modelo_final.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-url = "D:\3_Especialización_en_ciencia_de_datos\2. material de clase, cursos y materias\ciencia de datos\Proyecto_final_chrum\Untitled-1.txt"
+url = (r"D:\3_Especialización_en_ciencia_de_datos\2. material de clase, cursos y materias\ciencia de datos\Proyecto_final_chrum\Trabajo final 2 - Churn-20250329\Churn_Modelling\Churn_Modelling.csv")
 
-df_ppal = pd.read.txt(url)
+df_ppal = pd.read_csv(url)
 
-print(df_ppal)
+print(df_ppal.head)
