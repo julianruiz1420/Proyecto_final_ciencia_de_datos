208ee9fd6b93c64c8ebd219bb41d0671d6a96824
julianruiz1420 <julian.ruiz1420@gmail.com>
2025-04-06 19:09:12 -0500

    update readme.md

--- 1. Cargar_datos.py
+++ 1. Cargar_datos.py
@@ -1,14 +1,14 @@
-from pathlib import Path
+# Cambiar la dirección donde este alojada el csv
+
 import pandas as pd
 
-# Construir ruta relativa al archivo CSV
-ruta_csv = Path("Churn_Modelling/Churn_Modelling.csv")
 
-# Cargar el CSV
-df_ppal = pd.read_csv(ruta_csv)
-
+Url_csv = (r"D:\3_Especialización_en_ciencia_de_datos\2. material de clase, cursos y materias\ciencia de datos\Proyecto_final_chrum\data\Churn_Modelling.csv")
+           
+df_ppal = pd.read_csv(Url_csv)
 
 print(df_ppal.head)
 
 print(df_ppal.describe())
+
 print(df_ppal.info())
